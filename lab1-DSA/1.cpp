edb608557cb44fe9adadaaa006bdc5eea76bf785
One piece <157497306+Onepiece-7@users.noreply.github.com>
2025-05-21 13:39:49 +0545

    Update 1.cpp

--- lab1-DSA/1.cpp
+++ lab1-DSA/1.cpp
@@ -1,1 +1,18 @@
 
+// WAP to display n elements in 10 array
+
+#include <stdio.h>
+int main()
+{
+    int i, a[10];
+    printf("Enter the elements for the array:\n");
+    for (i = 0; i < 10; i++)
+    {
+        scanf("%d", &a[10]);
+    }
+    printf("The elements present in the array are:\n");
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\t", a[i]);
+    }
+}
